23c5bfb6119e4ed374c062696cdabba5a839c9f7
GabrielAugusto842 <gabrielmacchi.santos@gmail.com>
2026-04-12 17:13:58 -0300

    Criando classe Processo e Vetor Dinamico

---
+++ Processo.java
@@ -0,0 +1,61 @@
+public class Processo {
+    private int protocolo; //único, automático e sequencial
+    private String solicitante; //quem abriu o processo
+    private String tipoServico; // tipo de atendimento, saúde, financeiro
+    private int prioridade; //1 - baixa, 2 - normal, 3 - urgente
+    private String dataHora; //formato dd/MM/yyyy HH:mm
+
+    public Processo(String solicitante, String tipoServico, int prioridade) {
+        this.solicitante = solicitante;
+        this.tipoServico = tipoServico;
+        this.prioridade = prioridade;
+    }
+
+    public int getProtocolo() {
+        return protocolo;
+    }
+
+    public void setSolicitante (String solicitante) {
+        this.solicitante = solicitante;
+    }
+
+    public String getSolicitante() {
+        return solicitante;
+    }
+
+    public void setTipoServico (String tipoServico) {
+        this.tipoServico = tipoServico;
+    }
+
+    public String getTipoServico() {
+        return tipoServico;
+    }
+
+    public void setPrioridade (int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setDataHora (String dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getDataHora() {
+        return dataHora;
+    }
+
+    @Override
+    public String toString() {
+        return "[#" + protocolo + "] " + solicitante + " | " + tipoServico + " | " + prioridade + 
+        " | " + dataHora;
+    }
+
+    public Boolean ComparaProcesso (int protocolo) {
+        //if ((this.protocolo).equals(protocolo)) {
+            return true;
+        //}
+    }
+}
---
+++ VetorDinamico.java
@@ -0,0 +1,8 @@
+public class VetorDinamico {
+    private int capacidade = 4;
+    private Processo[] processo;
+
+    public VetorDinamico(int capacidade) {
+        this.capacidade = capacidade;
+    }
+}
